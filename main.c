d06eeda915d515f705b2a85d902442dbc097a562
Tomas Ramanauskas <tomasramanauskas@Tomass-MacBook-Pro.local>
2018-09-28 14:26:51 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int a;
+    int b;
+    int suma, sandauga;
+
+    printf("Įveskite pirmąjį skaičių:\n");
+    scanf("%d", &a);
+
+    printf("Įveskite antrąjį skaičių:\n");
+    scanf("%d", &b);
+
+    suma = a + b;
+    sandauga = a * b;
+
+    printf("Skaičių suma yra %d \nSkaičių sandauga yra %d", suma, sandauga);
+
+    return 0;
+}
